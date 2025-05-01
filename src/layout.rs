674a0547499ba64122f77462d1a07b85596d7c05
William Raendchen <william.raendchen@gmail.com>
2025-05-02 00:00:00 +0200

    update

--- src/layout.rs
+++ src/layout.rs
@@ -14,20 +14,36 @@ pub fn layout(state: &GameState) -> Vec<JobLayout> {
     let mut y_offset = 200.0;
 
     for (i, _job) in state.jobs.iter().enumerate() {
+        let card_padding = 20.0;
         let card_x = 50.0;
         let card_y = y_offset;
         let card_w = 400.0;
-        let card_h = 180.0;
+        let card_h = 175.0;
 
         layouts.push(JobLayout {
             job_index: i,
             card_rect: Rectangle::new(card_x, card_y, card_w, card_h),
-            button_rect: Rectangle::new(card_x + 200.0, card_y + 30.0, 100.0, 30.0),
-            action_bar_rect: Rectangle::new(card_x + 10.0, card_y + 140.0, 300.0, 20.0),
-            level_bar_rect: Rectangle::new(card_x + 10.0, card_y + 170.0, 300.0, 20.0),
+            button_rect: Rectangle{
+                x: card_x + card_w - card_padding - 90.0,
+                y: card_y + card_padding,
+                width: 90.0,
+                height: 40.0,
+            },
+            action_bar_rect: Rectangle{
+                x: card_x + card_padding,
+                y: card_y + card_padding + 80.0,
+                width: card_w - card_padding * 2.0,
+                height: 20.0,
+            },
+            level_bar_rect: Rectangle{
+                x: card_x + card_padding,
+                y: card_y + card_padding + 110.0,
+                width: card_w - card_padding * 2.0,
+                height: 20.0,
+            }
         });
 
-        y_offset += 240.0;
+        y_offset += 200.0;
     }
 
     layouts
