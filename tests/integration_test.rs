9974154f17cda86ebe2d27ecd0aa49c5c7223525
epic-64 <william.raendchen@gmail.com>
2025-05-06 00:00:00 +0200

    fix test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,4 +1,5 @@
 use tiny_fields::game::{GameState, Intent};
+use tiny_fields::game::Intent::ToggleJob;
 
 #[test]
 fn it_works() {
@@ -8,4 +9,17 @@ fn it_works() {
     game_state.step(intents.as_slice(), 0.016);
 
     assert_eq!(game_state.total_money, 0);
+}
+
+#[test]
+fn toggle_job() {
+    let mut game_state = GameState::new();
+
+    let intents = vec![ToggleJob(0)];
+
+    assert_eq!(game_state.jobs[0].running, false);
+
+    game_state.step(intents.as_slice(), 0.016);
+
+    assert_eq!(game_state.jobs[0].running, true);
 }
