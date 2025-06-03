64e84afca31fbc5d9267ecaaece6ea42ea17ac61
William Raendchen <william.raendchen@gmail.com>
2025-06-04 00:00:00 +0200

    cleanup

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,7 +1,4 @@
-use std::collections::HashSet;
-use strum::IntoEnumIterator;
 use tiny_fields::game::{GameState, Intent, Item};
-use tiny_fields::job::{JobArchetype, JobArchetypeInstances};
 
 #[test]
 fn it_works() {
@@ -11,24 +8,4 @@ fn it_works() {
     game_state.step(intents.as_slice(), 0.016);
 
     assert_eq!(game_state.inventory.get_item_amount(&Item::Coin), 0);
-}
-
-#[test]
-fn all_job_archetypes_are_instantiated() {
-    let job_instances = JobArchetypeInstances::new();
-
-    let actual: HashSet<_> = job_instances
-        .instances
-        .iter()
-        .map(|inst| inst.job_archetype)
-        .collect();
-
-    let expected: HashSet<_> = JobArchetype::iter().collect();
-
-    assert_eq!(
-        actual, expected,
-        "Mismatch between expected and actual job archetypes.\nMissing: {:?}\nExtra: {:?}",
-        expected.difference(&actual).collect::<Vec<_>>(),
-        actual.difference(&expected).collect::<Vec<_>>()
-    );
 }
